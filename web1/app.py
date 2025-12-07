218ee952528be8f180c54bf2a7c95b6dbad61dfb
HAFID AIDIL KAZUTBHA <159976644+Kochengku@users.noreply.github.com>
2025-12-08 05:48:38 +0700

    Add files via upload

--- web1/app.py
+++ web1/app.py
@@ -1306,7 +1306,6 @@ def is_panel_alive(panel_id, timeout=5):
         
 def sync_user_multi_panel(user):
     email = user.email
-    do_sync = True
 
     for panel_id in PANELS.keys():
 
@@ -1626,6 +1625,7 @@ def dashboard():
         if not session.get("sync_done"):
             print("[SYNC] Sinkronisasi pertama kali dilakukan")
             
+            do_sync = True
 
             sync_ok = sync_user_multi_panel(user)
 
