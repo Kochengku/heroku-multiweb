cc13a711a02da6ea9a85816fa1f4d42703a17d23
Kochengku <kochenggamnteng@gmail.com>
2025-12-27 09:59:28 +0700

    Sync local project (overwrite repo + add new files)

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import request
+from werkzeug.wrappers import Response
+
+# =====================================
+# IMPORT APP SETIAP DOMAIN
+# =====================================
+from web1.app import app as web1
+from web2.app import app as web2
+from web3.app import app as web3
+from web4.app import app as web4
+
+
+# =====================================
+# ROUTER DOMAIN KE FLASK APP
+# =====================================
+
+DOMAIN_MAP = {
+    "control.kocheng.biz.id": web1,
+    "kocheng.biz.id": web2,
+    "control.skyforgia.web.id": web3,
+    "skyforgia.web.id": web4
+}
+
+class HostDispatcher:
+    def __call__(self, environ, start_response):
+        host = environ.get("HTTP_HOST", "").split(":")[0].lower()
+
+        for domain, app in DOMAIN_MAP.items():
+            if domain in host:
+                return app(environ, start_response)
+
+        return Response("Domain tidak ditemukan", status=404)(environ, start_response)
+
+app = HostDispatcher()
